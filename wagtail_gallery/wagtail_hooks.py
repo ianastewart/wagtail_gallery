08480bc629acc04d18b7cca94331d4da666bc008
ianastewart <is@iskt.co.uk>
2025-01-15 16:14:06 +0000

    Migrate from ModelAdminViewSet

--- wagtail_gallery/wagtail_hooks.py
+++ wagtail_gallery/wagtail_hooks.py
@@ -1,33 +1,39 @@
 from django.utils.translation import gettext_lazy as _
-from wagtail.contrib.modeladmin.options import ModelAdmin, modeladmin_register, ModelAdminGroup
+
+from wagtail.snippets.models import register_snippet
+from wagtail.snippets.views.snippets import SnippetViewSet, SnippetViewSetGroup
+
 from .models import GalleryPage, Category
 
 
-class GalleryPageModelAdmin(ModelAdmin):
+class GalleryPageViewSet(SnippetViewSet):
     """Makes the adding of gallery pages easy by providing a hook to the menu system thus not needing to use the page hierarchy"""
+
     model = GalleryPage
-    menu_label = _('Gallery')
-    menu_icon = 'picture'
-    list_display = ('first_published_at', 'title', 'description', 'categories')
-    list_filter = ('first_published_at', 'categories')
-    search_fields = ('title', 'description', 'first_published_at')
+    menu_label = _("Gallery")
+    menu_icon = "picture"
+    list_display = ("first_published_at", "title", "description", "categories")
+    list_filter = ("first_published_at", "categories")
+    search_fields = ("title", "description", "first_published_at")
 
 
-class GalleryCategoryAdmin(ModelAdmin):
+class GalleryCategoryViewSet(SnippetViewSet):
     """Makes the adding of categories simply by giving own menu entry"""
+
     model = Category
-    menu_label = _('Categories')
-    menu_icon = 'group'
-    list_display = ('name',)
-    search_fields = ('name',)
+    menu_label = _("Categories")
+    menu_icon = "group"
+    list_display = ("name",)
+    search_fields = ("name",)
 
 
-class GalleryModelAdminGroup(ModelAdminGroup):
+class GalleryViewSetGroup(SnippetViewSetGroup):
     """Creates the side menu entry with the two other ModelAdmins inside of it"""
-    menu_label = _('Gallery')
-    menu_icon = 'picture'
+
+    menu_label = _("Gallery")
+    menu_icon = "picture"
     menu_order = 300
-    items = (GalleryPageModelAdmin, GalleryCategoryAdmin)
+    items = (GalleryPageViewSet, GalleryCategoryViewSet)
 
+register_snippet(GalleryViewSetGroup)
 
-modeladmin_register(GalleryModelAdminGroup)
